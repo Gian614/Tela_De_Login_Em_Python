27b4ef7fae043567ff84ead0cbefa87bf0843283
Gian_Kevin <56491903+Gian614@users.noreply.github.com>
2023-05-31 14:27:57 -0400

    Update TELA DE ACESSO.py

--- Pagina de login/TELA DE ACESSO.py
+++ Pagina de login/TELA DE ACESSO.py
@@ -3,9 +3,25 @@ import webbrowser
 import time
 
 def abrir_youtube():
-    webbrowser.open("https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0")
-    master.destroy()
-    time.sleep(0.3)
+    email = en_email.get()
+    password = en_senha.get()
+    
+    
+    if email == "teste@gmail.com" and password == "123456":
+        webbrowser.open("https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0")
+        master.destroy()
+        time.sleep(0.3)
+    else:
+        
+        messagebox.showerror("Error", "Invalid email or password")
+
+def create_email():
+    email = en_email.get()
+   
+
+def create_password():
+    password = en_senha.get()
+    
 
 master = Tk()
 master.title("Portal de alunos")
@@ -14,7 +30,6 @@ master.iconbitmap(default="icones\\ico.ico")
 master.resizable(width=1, height=1)
 
 esconda_senha = StringVar()
-
 img_fundo = PhotoImage(file="imagens\\fundo.png")
 img_botao = PhotoImage(file="imagens\\LOGIN.png")
 
@@ -33,5 +48,11 @@ en_senha.place(width=425, height=85, x=32, y=320)
 
 bt_entrar = Button(master, bd=0, image=img_botao, command=abrir_youtube)
 bt_entrar.place(width=118, height=64, x=186, y=489)
+
+bt_create_email = Button(master, text="teste@gmail.com", command=create_email)
+bt_create_email.pack()
+
+bt_create_password = Button(master, text="123456", command=create_password)
+bt_create_password.pack()
 
 master.mainloop()
